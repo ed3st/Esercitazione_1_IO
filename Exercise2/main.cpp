f63980589dbd2b703ba4221fea04b51f743ff04d
ed3st <s312932@studenti.polito.it>
2025-03-30 22:35:53 +0200

    correzzione main

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -16,7 +17,7 @@ int main()
 	string ifilename = "data.txt";
 	ifstream ifs(ifilename); 
 	if( ifs.fail() ) { // controllo se l'apertura è andata a buon fine
-		cerr << "Errore nell'apertura del file di input" << endl;
+		cerr << "Errore nell'apertura del file " << ifilename << endl;
 		return 1;
 	}
 	
@@ -24,7 +25,7 @@ int main()
 	string ofilename = "result.txt"; 
 	ofstream ofs(ofilename);
 	if( ofs.fail() ) { //controllo se l'apertura è andata a buon fine
-		cerr << "Errore nell'apertura del file di output" << endl;
+		cerr << "Errore nell'apertura del file " << ofilename << endl;
 		return 2;
 	}
     
@@ -34,15 +35,16 @@ int main()
 	/*leggo uno alla volta i valori in "data.txt", 
 	li mappo nell'intervallo [-1,2] tramite f_affine,
 	calcolo di volta in volta la media dei primi i valori 
-	e stampo il risultato in "result.txt nel formato richiesto." */
+	e stampo il risultato in "result.txt" nel formato richiesto. */
 	double val;
 	unsigned int i = 1; //inizializzo a 1 un contatore intero
 	double sum = 0.0; //inizializzo la somma a 0
 	while(ifs>>val)
 	{
 		sum += f_affine(val); //mappo il valore appena letto in [-1,2] e lo aggiungo alla somma
-		ofs << i << " " << setprecision(16) << scientific << sum/(i++) << endl; 
-		//stamo il contatore e la media dei primi i valori mappati. aggiorno il contatore.
+		ofs << i << " " << setprecision(16) << scientific << sum/(i) << endl; 
+		//stampo il contatore e la media dei primi i valori mappati. 
+		i++; //aggiorno il contatore.
 	}
 	
 	ifs.close(); //chiudo "data.txt"
